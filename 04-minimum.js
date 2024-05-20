ccd2ad7c9bbf96e7a243da7a5c510c1b898f804a
Achap05 <chapon.art@gmail.com>
2024-05-21 00:04:48 +0200

    add 04-minimum.js

---
+++ 04-minimum.js
@@ -0,0 +1,17 @@
+/*
+Write a function min that takes two arguments and returns their minimum.
+
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+*/
+
+function getMin( a, b ) {
+    if( a < b) {
+        return a;
+    } else {
+        return b
+}
+}
+
