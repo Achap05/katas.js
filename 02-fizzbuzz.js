42c4e86432e146da98f5cde713937909288f0caa
Achap05 <chapon.art@gmail.com>
2024-05-21 11:47:36 +0200

    edit 02-fizzbuzz.js

--- 02-fizzbuzz.js
+++ 02-fizzbuzz.js
@@ -9,11 +9,27 @@ modify your program to print "FizzBuzz" for numbers that are divisible by both 3
 */
 
 
-for ( i = 1, i <= 100; i++ ;) {
+for ( i = 1; i <= 100; i++ ) {
     if ( i % 3 == 0 ) {
-     console.log ( "Fizz") ;
+     console.log ( "Fizz" ) ;
+     continue;
     } else if ( i % 5 == 0 ) {
         console.log ( "Buzz" ) ;
+        continue;
+    } console.log ( i );
+}
 
+
+for ( i = 1; i <= 100; i++ ) {
+    if ( i % 3 == 0 && i % 5 ==0) {
+        console.log ( "Fizzbuzz");
+        continue;
+    }
+    else if ( i % 3 == 0 ) {
+     console.log ( "Fizz" ) ;
+     continue;
+    } else if ( i % 5 == 0 ) {
+        console.log ( "Buzz" ) ;
+        continue;
     } console.log ( i );
 }
